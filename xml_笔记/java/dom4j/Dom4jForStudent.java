833705f2bf8f507ffc5684d427b1cd4a35f10720
Administrator <1746678613@qq.com>
2020-12-11 17:38:58 +0800

    bran1 web|xml 创建xml并用流写入到模块中，asXML()

--- xml_笔记/java/dom4j/Dom4jForStudent.java
+++ xml_笔记/java/dom4j/Dom4jForStudent.java
@@ -7,6 +7,8 @@ import org.dom4j.io.SAXReader;
 import org.junit.Test;
 import pojo.Student;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.List;
 
 /**
@@ -57,9 +59,50 @@ public class Dom4jForStudent {
         return document;
     }
 
-    public static void main(String[] args) {
+    public Document createDoc(){
+        Document document = DocumentHelper.createDocument();
+        Element students = document.addElement("students");
+        Element student1 = students.addElement("student");
+        student1.addAttribute("num","001");
+        student1.addElement("name").addText("狼崽");
+        student1.addElement("age").addText("20");
+
+        Element student2 = students.addElement("student");
+        student2.addAttribute("num","002");
+        student2.addElement("name").addText("星儿");
+        student2.addElement("age").addText("18");
+
+
+
+        return document;
+    }
+
+    public static void main(String[] args) throws Exception{
         Dom4jForStudent dom4jForStudent = new Dom4jForStudent();
         Document document = dom4jForStudent.createDocument();
-        System.out.println(document.asXML());
+//      System.out.println(document.asXML());
+        Document doc = dom4jForStudent.createDoc();
+        FileOutputStream outputStream=new FileOutputStream("E:\\java_web\\xml_笔记\\java\\test.xml");
+        String s = doc.asXML();
+
+        char flag='>';
+        StringBuffer buffer1=new StringBuffer();
+        for (int i = 0; i < s.length(); i++) {
+            buffer1.append(s.charAt(i));
+            if(s.charAt(i)==flag){
+            buffer1.append('\n');
+
+
+            }
+        }
+
+        String s1 = buffer1.toString();
+        System.out.println(s1);
+        byte[] bytes = s1.getBytes();
+       outputStream.write(bytes,0,bytes.length);
+
+
+        System.out.println(doc.asXML());
+
     }
 }
