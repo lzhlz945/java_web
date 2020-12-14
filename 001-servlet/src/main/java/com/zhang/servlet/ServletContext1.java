0091b0bc50a53e8e16a284cc67d145f04f77d4b5
Administrator <1746678613@qq.com>
2020-12-14 13:33:47 +0800

    bran1 web|ServletContext作为域传递数据和请求和响应头说明

---
+++ 001-servlet/src/main/java/com/zhang/servlet/ServletContext1.java
@@ -0,0 +1,31 @@
+package com.zhang.servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author: create by zhl
+ * @version: v1.0
+ * @description: ${PACKAGE_NAME}
+ * @date:2020/12/14
+ */
+@WebServlet(name = "ServletContext1")
+public class ServletContext1 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        ServletContext context = request.getServletContext();
+        System.out.println("之前获取servletContext中name的值："+context.getAttribute("name"));
+        context.setAttribute("name","星儿");
+
+    }
+}
