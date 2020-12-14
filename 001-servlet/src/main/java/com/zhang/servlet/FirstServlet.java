a0eb0e0abb2d9aaf933c072f71a20032e96f1af3
Administrator <1746678613@qq.com>
2020-12-14 10:36:46 +0800

    bran1 web|servlet的继承关系图

--- 001-servlet/src/main/java/com/zhang/servlet/FirstServlet.java
+++ 001-servlet/src/main/java/com/zhang/servlet/FirstServlet.java
@@ -16,11 +16,23 @@ import java.io.IOException;
 @WebServlet(name = "FirstServlet")
 public class FirstServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("hello|doPost");
+    }
 
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String method = req.getMethod();
+        if(method.equals("doPost")){
+            System.out.println("dopost方法被调用了");
+        }else {
+            System.out.println("doget方法被调用了");
+
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        System.out.println("hello");
+        System.out.println("hello|doGet");
     }
 }
