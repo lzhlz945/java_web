c31bc18d487942ce2488275bd33e8869183f5854
Administrator <1746678613@qq.com>
2020-12-21 10:30:12 +0800

    bran1 json|json字符和list互转 type，利用typeToken的匿名内部内

--- jsonweb/src/main/java/com.zhang/TestJson.java
+++ jsonweb/src/main/java/com.zhang/TestJson.java
@@ -1,8 +1,12 @@
 package com.zhang;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.zhang.proj.Person;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author: create by zhl
@@ -12,6 +16,7 @@ import org.junit.jupiter.api.Test;
  */
 public class TestJson {
 
+    //json字符和javabean互转
     @Test
     public void test01(){
 
@@ -23,4 +28,22 @@ public class TestJson {
         System.out.println(person1);
     }
 
+    //json字符和list互转
+    @Test
+    public void test02(){
+        List<Person> personList = new ArrayList<Person>();
+        personList.add(new Person(1, "123"));
+        personList.add(new Person(2, "1234"));
+        Gson gson=new Gson();
+        String json = gson.toJson(personList);
+        System.out.println(json);
+
+        List<Person> listPerson = gson.fromJson(json, new TypeToken<ArrayList<Person>>() {
+        }.getType());
+        System.out.println(listPerson);
+    }
+    @Test
+    public void test03(){
+
+    }
 }
